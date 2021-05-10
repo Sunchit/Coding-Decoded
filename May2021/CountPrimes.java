db7c98d5766250746145f47e914309d9c647c716
Sunchit Dudeja <sunchitdudeja@gmail.com>
2021-05-10 22:44:24 +0530

    CountPrimes2

--- May2021/CountPrimes.java
+++ May2021/CountPrimes.java
@@ -16,3 +16,24 @@ class Solution {
         return count;
     }
 }
+// Solution 2 where inner loop is not starting from 2
+class Solution {
+
+    // TC : O(nloglogn)
+    // SC : O(n)
+    public int countPrimes(int n) {
+        boolean[] noList = new boolean[n];
+        int count = 0;
+        for(int i=2;i<n;i++){
+            if(noList[i] == false){ // i no is prime
+                count++;
+                for(int factor=i;factor<n;){
+
+                    noList[factor] = true; // factor is marked non prime
+                    factor = factor + i;
+                }
+            }
+        }
+        return count;
+    }
+}
