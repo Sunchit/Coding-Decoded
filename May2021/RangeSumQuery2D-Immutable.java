0d4e83b45a419d27618aa680bbf184bc8ff7deb9
Sunchit Dudeja <sunchitdudeja@gmail.com>
2021-05-12 16:16:54 +0530

    RangeSumQuery2D-Immutable

---
+++ May2021/RangeSumQuery2D-Immutable.java
@@ -0,0 +1,30 @@
+class NumMatrix {
+
+    private int[][] dp;
+
+    // O(n*m)
+    public NumMatrix(int[][] matrix) {
+        if(   matrix           == null
+                || matrix.length    == 0
+                || matrix[0].length == 0   ){
+            return;
+        }
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        dp = new int[m + 1][n + 1];
+
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] -dp[i - 1][j - 1] + matrix[i - 1][j - 1] ;
+            }
+        }
+
+    }
+
+    // TC O(1)
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return dp[row2 + 1][col2 + 1] - dp[row1][col2 + 1] - dp[row2 + 1][col1] + dp[row1][col1];
+    }
+}
