547d723b21a966e67c02e99bd7732a41fafecadc
Sunchit Dudeja <sunchitdudeja@gmail.com>
2021-06-03 18:26:11 +0530

    test

--- June2021/MaximumAreaofaPieceofCakeAfterHorizontalandVerticalCuts.java
+++ June2021/MaximumAreaofaPieceofCakeAfterHorizontalandVerticalCuts.java
@@ -1,21 +1,21 @@
 class Solution {
+
+    // TC : O(vlogv + hlogh)
     public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
 
         Arrays.sort(horizontalCuts);
         Arrays.sort(verticalCuts);
 
-        int hLength = horizontalCuts.length;
-        int vLength = verticalCuts.length;
 
-        int maxHeight = Math.max(horizontalCuts[0]-0, h - horizontalCuts[hLength-1]);
-        for (int i = 1;i<hLength; i++){
+        int maxHeight = Math.max(horizontalCuts[0]-0, h - horizontalCuts[horizontalCuts.length-1]);
+        for (int i = 1;i<horizontalCuts.length; i++){
             int horizatalWidth = horizontalCuts[i] - horizontalCuts[i-1];
             maxHeight = Math.max(maxHeight, horizatalWidth);
         }
 
 
-        int maxWidth = Math.max(verticalCuts[0]-0, w - verticalCuts[vLength-1]);
-        for (int i = 1;i<vLength; i++){
+        int maxWidth = Math.max(verticalCuts[0]-0, w - verticalCuts[verticalCuts.length-1]);
+        for (int i = 1;i<verticalCuts.length; i++){
             int verticalWidth = verticalCuts[i] - verticalCuts[i-1];
             maxWidth = Math.max(maxWidth, verticalWidth);
         }
