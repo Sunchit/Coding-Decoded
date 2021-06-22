c95eb0b790d52fa6068a42ff3d6f5b12fea8e91f
Aditya chauhan <62752956+adibalveer@users.noreply.github.com>
2021-06-22 18:34:17 +0530

    Update Pascal's Triangle-Day 21.py

--- June2021/python/Pascal's Triangle-Day 21.py
+++ June2021/python/Pascal's Triangle-Day 21.py
@@ -8,7 +8,7 @@ class Solution:
             lis=[0 for _ in range(j)]
             lis[0],lis[-1]=1,1
             for i in range(1,len(lis)-1):
-                # if i>0 and i <len(lis)-1:
+               
                 lis[i]=new_lis[-1][i-1]+new_lis[-1][i]
             
             new_lis+=[lis]
