132fb3643c3a3f8f1dfbe4d745811e1c948743ae
Sunchit Dudeja <sunchitdudeja@gmail.com>
2021-07-09 18:22:13 +0530

    Update LongestIncreasingSubsequence.java

--- July2021/LongestIncreasingSubsequence.java
+++ July2021/LongestIncreasingSubsequence.java
@@ -1,19 +1,41 @@
 class Solution {
-
-    // TC : O(nlogn)
-    // SC: O(n)
-    public int lengthOfLIS(int[] nums) {
-        TreeSet<Integer> bst = new TreeSet<>();
-        for(int num: nums){
-            Integer higherOrEqual = bst.ceiling(num);
-            if(higherOrEqual == null){
-                bst.add(num);
-            } else{
-                bst.remove(higherOrEqual);
-                bst.add(num); // O(logn)
-            }
-        }
-
-        return bst.size();
+    //TC: O(n2)
+    // SC : O(n)
+  public int lengthOfLIS(int[] nums) {
+    if (nums == null || nums.length == 0) {
+      return 0;
     }
+    int[] dp = new int[nums.length];
+    int max = 1;
+    for (int i = 0; i < nums.length; i++) {
+      dp[i] = 1;
+      for (int j = 0; j < i; j++) {
+        if (nums[j] < nums[i]) {
+          dp[i] = Math.max(dp[i], dp[j] + 1);
+          max = Math.max(dp[i], max);
+        }
+      }
+    }
+    return max;
+  }
+
+  // TC : O(nlogn)
+  // SC: O(n)
+  public int lengthOfLIS(int[] nums) {
+    TreeSet<Integer> bst = new TreeSet<>();
+    for (int num : nums) {
+      Integer higherOrEqual = bst.ceiling(num);
+      if (higherOrEqual == null) {
+        bst.add(num);
+      }
+      else {
+        bst.remove(higherOrEqual);
+        bst.add(num); // O(logn)
+      }
+    }
+
+    return bst.size();
+  }
 }
+
+
