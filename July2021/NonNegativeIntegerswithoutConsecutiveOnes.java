0c4a2b263d3efa576003a89c6e30fa8712b0cf73
santhosh-sethu-99 <76466079+santhosh-sethu-99@users.noreply.github.com>
2021-07-26 08:49:14 +0530

    Java solution for july 26!

--- July2021/NonNegativeIntegerswithoutConsecutiveOnes.java
+++ July2021/NonNegativeIntegerswithoutConsecutiveOnes.java
@@ -24,3 +24,43 @@ class Solution {
     return ans+1;
   }
 };
+
+
+
+// This is Java Solution
+class Solution {
+    public int findIntegers(int n) {
+        int[] f = new int[31];
+        int res = 0;
+        
+        f[0] = 1;
+        f[1] = 2;
+        for(int i = 2; i < 31; i++){
+            f[i] = f[i-1] + f[i-2];
+        }
+        
+        String binaryStr = getBinary(n);
+        
+        for(int i = 0; i < binaryStr.length(); i++){
+            if(binaryStr.charAt(i) == '0')
+                continue;
+            
+            res += f[binaryStr.length() - i - 1];
+            if(i != 0 && binaryStr.charAt(i-1) == '1')
+                return res;
+        }
+        
+        return res+1;
+    }
+    
+    public String getBinary(int num){
+        StringBuilder sb = new StringBuilder();
+        
+        while(num > 0){
+            sb.insert(0, num&1);
+            num >>= 1;
+        }
+        
+        return sb.toString();
+    }
+}
