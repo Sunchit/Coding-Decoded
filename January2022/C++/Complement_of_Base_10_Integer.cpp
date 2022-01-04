e2fd14953fae0ad11a009990856ca9a3e2837751
Ankit Kumar Ghosh <76126678+heyankit78@users.noreply.github.com>
2022-01-04 15:47:39 +0530

    C++ code

--- January2022/C++/Complement_of_Base_10_Integer.cpp
+++ January2022/C++/Complement_of_Base_10_Integer.cpp
@@ -12,3 +12,27 @@ public:
         
     }
 };
+
+
+// C++ code
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if(n==0)return 1;
+        int p = 1;
+        int ans= 0;
+        while(n){
+            int bit = n & 1;   //to find the last bit is 0 or 1 
+            if(bit==0){
+                //because 0 bit will contribute to our answer as we want compliment
+                ans+=p;
+            }
+            p <<= 1;   // p = p*2
+            n >>= 1;   // right shifting by 1
+        }
+        return ans;
+    }
+};
+
+
+// Contributed by Ankit Ghosh
