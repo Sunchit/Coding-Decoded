ae7fcbf90d49b06af1e1685d7751c5f0a1959e48
karthik1117 <58143966+karthik1117@users.noreply.github.com>
2022-01-14 13:32:07 +0530

    ss

--- January2022/C++/minimum_number_of_arrows_to_burst_balloons.cpp
+++ January2022/C++/minimum_number_of_arrows_to_burst_balloons.cpp
@@ -1,14 +1,17 @@
-class Solution {
+class Solution
+{
 public:
-    int findMinArrowShots(vector<vector<int>>& points) {
-        sort(points.begin(),points.end());
-        int r=points[0][1],ans=1;
-        for(auto x : points)
-            if(x[0]<=r)
-                r=min(r,x[1]);
-            else{
+    int findMinArrowShots(vector<vector<int>> &points)
+    {
+        sort(points.begin(), points.end());
+        int r = points[0][1], ans = 1;
+        for (auto x : points)
+            if (x[0] <= r)
+                r = min(r, x[1]);
+            else
+            {
                 ans++;
-                r=x[1];
+                r = x[1];
             }
         return ans;
     }
