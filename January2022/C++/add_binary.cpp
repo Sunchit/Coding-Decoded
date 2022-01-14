7682b967f2e524f5a4ca541b880957fc869d7ef3
karthik1117 <58143966+karthik1117@users.noreply.github.com>
2022-01-14 13:13:09 +0530

    January Leetcode Challenge - Day 14

--- January2022/C++/add_binary.cpp
+++ January2022/C++/add_binary.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        int al=a.size(),bl=b.size();
-        if(al<bl)
-            return addBinary(b,a);
-        int prev=0;
-        string ans;
-        for(int i=al-1;i>=0;i--){
-            int a1 = a[i]-'0';
-            int b1 = (i>=al-bl)?(b[i-al+bl]-'0'):0;
-            int temp = (prev + a1 + b1)%2;
-            prev = (prev + a1 + b1)/2;
-            ans+=(temp+'0');
+        string res="";
+        int c=0,i=a.length()-1,j=b.length()-1;
+        while(c || i>=0 || j>=0)
+        {
+            c+= i>=0? a[i--]-'0':0;
+            c+= j>=0? b[j--]-'0':0;
+            res=char(c%2 +'0')+res;  
+            c/=2;
         }
-        if(prev)
-            ans+='1';
-        return string(ans.rbegin(),ans.rend());
+        return res;
+    
     }
 };
