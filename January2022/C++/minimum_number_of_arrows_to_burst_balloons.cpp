aaabe0056299b5fe18f6e2be430cd3f06afa8ea2
karthik1117 <58143966+karthik1117@users.noreply.github.com>
2022-01-15 11:40:38 +0530

    January Leetcode Challenge - Day 15

--- January2022/C++/minimum_number_of_arrows_to_burst_balloons.cpp
+++ January2022/C++/minimum_number_of_arrows_to_burst_balloons.cpp
@@ -1,18 +1,24 @@
-class Solution
-{
+class Solution {
 public:
-    int findMinArrowShots(vector<vector<int>> &points)
-    {
-        sort(points.begin(), points.end());
-        int r = points[0][1], ans = 1;
-        for (auto x : points)
-            if (x[0] <= r)
-                r = min(r, x[1]);
-            else
+    int findMinArrowShots(vector<vector<int>>& points) {
+        if(points.size()==1) return 1;
+        sort(begin(points),end(points));
+        int start=points[0][0],end=points[0][1],res=1;
+        for(int i=1;i<points.size();i++)
+        {
+            if(points[i][0]<=end)
             {
-                ans++;
-                r = x[1];
+                start=max(start,points[i][0]);
+                end=min(end,points[i][1]);
             }
-        return ans;
+            else 
+            {
+                res++;
+                start=points[i][0];
+                end=points[i][1];
+            }
+        }
+        return res;
+        
     }
 };
