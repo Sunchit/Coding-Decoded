aee40cb9e4e9680cc77e00712add1b4ace246ccf
karthik1117 <58143966+karthik1117@users.noreply.github.com>
2022-01-19 16:52:36 +0530

    unneccessary

--- January2022/C++/can_place_flowers.cpp
+++ January2022/C++/can_place_flowers.cpp
@@ -1,19 +1,21 @@
+created by @pranav108
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        if(!n) return true;
-        for(int i=0;i<flowerbed.size() && n;i++)
-        {
-           if(!flowerbed[i])
-           {
-               if( (i==0 || !flowerbed[i-1]) && (i==flowerbed.size()-1 ||!flowerbed[i+1]))
-               {
-                   n--;
-                   flowerbed[i++]=1;
-                   
-               }
-           }
+        int temp=0,i=0,len=flowerbed.size();
+        while(i<len&&flowerbed[i]==0)
+            i++;
+        if(i==len)
+            return (len+1)/2>=n;
+        n-=i/2;
+        for(;i<len&&n>0;i++){
+            if(flowerbed[i]){
+                n-=temp>2?(temp-1)/2:0;
+                    temp=0;
+            }else
+                temp++;
         }
-        return !n;
+        n-=temp/2;
+        return n<=0;
     }
 };
