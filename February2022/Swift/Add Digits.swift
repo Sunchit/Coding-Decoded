b99d77a9239234a65cb1f0b8c70b9c05e4090c69
Deepal <deepalpatel94@gmail.com>
2022-02-08 23:36:34 +0530

    Add Digits: Swift

---
+++ February2022/Swift/Add Digits.swift
@@ -0,0 +1,56 @@
+// Author: Deepal
+// Problem: Add Digits
+// Link: https://leetcode.com/problems/add-digits/
+// Category: Easy
+// DataStructure: String
+
+private func helperAddDigits(_ num: Int) -> Int {
+    var num = num
+    var result = 0
+    
+    while num != 0 {
+        result += num % 10
+        num /= 10
+    }
+    return result
+}
+
+private func helperAddDigitsString(_ num: Int) -> Int {
+    var result: Int = 0
+    let string: String = num.description + ""
+    
+    for item in string {
+        result += Int(String(item))!
+    }
+    return Int(result)
+}
+
+func addDigits(_ num: Int) -> Int {
+    var result = num
+    while true {
+        result = helperAddDigitsString(result)
+        if result < 10 {
+            break
+        }
+    }
+    return result
+}
+
+// TC: O(Log N)
+// SC: O(1)
+
+/*
+ 
+ Example 1:
+ Input: num = 38
+ Output: 2
+ Explanation: The process is
+ 38 --> 3 + 8 --> 11
+ 11 --> 1 + 1 --> 2
+ Since 2 has only one digit, return it.
+ 
+ Example 2:
+ Input: num = 0
+ Output: 0
+ 
+ */
