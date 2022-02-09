9c3a6cb4ec657addc04c566ef8414f53f9ed9f41
Sunchit Dudeja <sunchitdudeja@gmail.com>
2022-02-09 15:59:56 +0530

    Update KdiffPairsinanArray.java

--- February2022/Java/KdiffPairsinanArray.java
+++ February2022/Java/KdiffPairsinanArray.java
@@ -1,27 +1,37 @@
-public class Solution {
-	// TC : O(n)
-	// SC : O(n)
-	public int findPairs(int[] nums, int k) {
-		if (nums == null || nums.length == 0 || k < 0)   return 0;
 
-		Map<Integer, Integer> map = new HashMap<>();
-		int count = 0;
-		for (int i : nums) {
-			map.put(i, map.getOrDefault(i, 0) + 1);
-		}
+// @saorav21994
+// TC:  O(n)
+// Medium
 
-		for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-			if (k == 0) {
-				if (entry.getValue() >= 2) {
-					count++;
-				}
-			} else {
-				if (map.containsKey(entry.getKey() + k)) {
-					count++;
-				}
-			}
-		}
+/*
+  Similar approach as sum of 2 nummber to a given sum in an array. Special case of (k == 0) -> check for frequency of the number >= 2 as only (x-x) will give 0.
+*/
 
-		return count;
-	}
+
+class Solution {
+    public int findPairs(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int n : nums) {
+        	map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+        
+        int pairs = 0;
+        
+        for (Integer key : map.keySet()) {
+        	int mapVal = map.getOrDefault(key+k, 0);
+            if (k == 0) {
+                if (mapVal > 1)
+                    pairs += 1;
+            }
+            else {
+                if (mapVal > 0)
+                    pairs += 1;
+            }
+        	
+        }
+        
+        return pairs;
+        
+    }
 }
