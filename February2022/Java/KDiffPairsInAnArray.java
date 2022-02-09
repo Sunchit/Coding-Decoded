aed02189b48d2057199ee6a2861c0602d351083b
romitdutta10 <46403346+romitdutta10@users.noreply.github.com>
2022-02-09 19:24:46 +0530

    Update KDiffPairsInAnArray.java

--- February2022/Java/KDiffPairsInAnArray.java
+++ February2022/Java/KDiffPairsInAnArray.java
@@ -69,3 +69,37 @@ class Solution {
         return pairs.size();
     }
 }
+
+// Problem : https://leetcode.com/problems/k-diff-pairs-in-an-array/
+// @romitdutta10
+// TC : O(nlogn)
+// Solution with O(1) space 
+
+class Solution {
+    public int findPairs(int[] nums, int k) {
+        Arrays.sort(nums);
+        int left=0,right=1;
+        int ans=0;
+        
+        while(left<nums.length && right<nums.length)
+        {
+            if(left==right || nums[right] - nums[left] < k)
+                right++;
+            
+            else if(nums[right] - nums[left] > k)
+                left++;
+            else
+            {
+                left++;
+                ans++;
+                while(left<nums.length && nums[left] == nums[left-1])
+                {
+                    left++;
+                }
+            }
+            
+            
+        }
+        return ans;
+    }
+}
