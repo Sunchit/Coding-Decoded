86ee579b9777887c2dd733e39304ddaee34e6dd3
Sunchit Dudeja <sunchitdudeja@gmail.com>
2022-02-11 12:28:54 +0530

    Update PermutationInString.java

--- February2022/Java/PermutationInString.java
+++ February2022/Java/PermutationInString.java
@@ -1,4 +1,70 @@
 
+
+class Solution {
+    public boolean checkInclusion(String t, String s) {
+        List<Integer> ans = new ArrayList<>();
+
+        int unmatchingCharCount = t.length();
+
+        if(t.length()>s.length()){
+            return false;
+        }
+
+        int[] freqCount = new int[26];
+
+        for(int i=0;i<t.length();i++){
+            int index = t.charAt(i)-'a';
+            freqCount[index]++;
+        }
+
+        int start =0;
+        int end =0;
+        for(;end<t.length();end++) {
+            int index = s.charAt(end)-'a';
+            if(freqCount[index]>0){
+
+                unmatchingCharCount--;
+            }
+            freqCount[index]--;
+
+        }
+        if(unmatchingCharCount==0){
+            return true;
+
+        }
+        for(;end<s.length();){
+            // remove starting index
+            int indexStart = s.charAt(start) -'a';
+
+            if(freqCount[indexStart]>=0) {
+                // char was present in t
+                unmatchingCharCount++;
+            }
+
+            freqCount[indexStart]++;
+
+            start++;
+
+            int indexEnd = s.charAt(end)-'a';
+            if(freqCount[indexEnd]>0){
+                unmatchingCharCount--;
+            }
+            freqCount[indexEnd]--;
+
+            end++;
+
+            if(unmatchingCharCount==0){
+                return true;
+            }
+
+        }
+
+        return false;
+
+    }
+
+}
+
 // @saorav21994
 // TC : O(l1+l1+(l2-l1)) = O(l1+ l2)
 // SC : O(26+26) = O(52) = Constant
