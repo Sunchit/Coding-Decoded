88d78eace4cf1ceae57d0e4a76933a50674aab20
karthik1117 <58143966+karthik1117@users.noreply.github.com>
2022-02-16 07:19:54 +0530

    February Leetcode Challenge - Day 16

---
+++ February2022/Python/SwapNodesinPairs.py
@@ -0,0 +1,18 @@
+# TC : O(N)
+# SC : O(1)
+
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        dummy = ListNode(0)
+        dummy.next = head
+        curr = dummy
+        while curr.next and curr.next.next:
+            first = curr.next
+            second = curr.next.next
+            curr.next = second
+            first.next = second.next
+            second.next = first
+            curr = curr.next.next
+        return dummy.next
