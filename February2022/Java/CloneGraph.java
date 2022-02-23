778a5399e6dd7e4869af9e360f400a8638d230f6
romitdutta10 <46403346+romitdutta10@users.noreply.github.com>
2022-02-23 19:32:26 +0530

    Update CloneGraph.java

--- February2022/Java/CloneGraph.java
+++ February2022/Java/CloneGraph.java
@@ -49,3 +49,119 @@ class Solution {
         return map.get(node);       // return the head
     }
 }
+
+// Author : @romitdutta10
+// SC : O(n)
+// TC : O(total node references in original graph)
+// DFS easy to understand
+// Problem : https://leetcode.com/problems/clone-graph/
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    Map<Node, Node> nodePool = new HashMap<>();
+    public Node cloneGraph(Node node) {
+        if(node == null) {
+            return null;
+        }
+        
+        if(nodePool.containsKey(node)) {
+            return nodePool.get(node);
+        }
+        
+        Node copy = new Node(node.val);
+        nodePool.put(node, copy);
+        
+        List<Node> neighbours = new ArrayList<>();
+        
+        for(Node neighbour : node.neighbors) {
+            Node copiedNeighbour = cloneGraph(neighbour);
+            neighbours.add(copiedNeighbour);
+        }
+        
+        copy.neighbors = neighbours;
+        
+        
+        return copy;
+        
+        
+    }
+}
+
+// Author : @romitdutta10
+// SC : O(n)
+// TC : O(total node references in original graph)
+// DFS optimized
+// Problem : https://leetcode.com/problems/clone-graph/
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    HashMap<Integer, Node> nodeMap = new HashMap<>();
+    public Node cloneGraph(Node node) {
+        if(node == null)
+            return null;
+        
+        Node new_node;
+        if(nodeMap.get(node.val)!=null)
+            new_node = nodeMap.get(node.val);
+        else {
+            //System.out.println("Adding new node "+node.val);
+            new_node = new Node(node.val);
+            nodeMap.put(node.val, new_node);
+        }
+
+        List<Node> neighbors = new_node.neighbors;
+        for(Node n : node.neighbors){
+            //System.out.println("Traversing over neighbors "+n.val);
+            if(!neighbors.contains(n)){
+                if(nodeMap.get(n.val)!=null)
+                    neighbors.add(nodeMap.get(n.val));
+                else
+                    neighbors.add(cloneGraph(n));
+            }
+        }
+        
+
+        return new_node;
+    }
+}
