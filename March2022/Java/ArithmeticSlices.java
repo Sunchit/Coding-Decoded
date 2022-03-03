1318a46aa8dbfb6066ec9ff11306f28c337c8639
Sunchit Dudeja <sunchitdudeja@gmail.com>
2022-03-03 11:47:51 +0530

    Day3

--- March2022/Java/ArithmeticSlices.java
+++ March2022/Java/ArithmeticSlices.java
@@ -58,3 +58,39 @@ class Solution {
         return c;
     }
 }
+// @coding decoded
+class Solution {
+
+    // TC : O(n)
+    // SC : O(n)
+    public int numberOfArithmeticSlices(int[] A) {
+        int[] dp = new int[A.length];
+        int total = 0;
+        for(int i=2;i<A.length;i++){
+            // Third- Second == Second-First
+            if(A[i] - A[i-1] == A[i-1] - A[i-2]){
+                dp[i] = dp[i-1]+1;
+            } else{
+                dp[i] = 0;
+            }
+            total += dp[i];
+        }
+        return total;
+    }
+
+    // TC: O(n)
+    // SC: O(1)
+    public int numberOfArithmeticSlices(int[] A) {
+        int previousCount = 0;
+        int total = 0;
+        for(int i=2;i<A.length;i++){
+            if(A[i] - A[i-1] == A[i-1] - A[i-2]){
+                previousCount = previousCount +1;
+            } else{
+                previousCount = 0;
+            }
+            total += previousCount;
+        }
+        return total;
+    }
+}
--- README.md
+++ README.md
@@ -150,6 +150,7 @@ S.NO| Code Link            | Video Link
 :-------------------------:|:-------------------------:|:-------------------------:
 1 |[CountingBits](/March2022/Java/CountingBits.java)  | <a href="https://www.youtube.com/watch?v=uAcxY2w-oGI"><img alt="Solution" src="https://github.com/Sunchit/Coding-Decoded/blob/master/img/Video.png" height="20" width="20"></a>
 1 |[IsSubsequence](/March2022/Java/IsSubsequence.java)  | <a href="https://www.youtube.com/watch?v=uAcxY2w-oGI"><img alt="Solution" src="https://github.com/Sunchit/Coding-Decoded/blob/master/img/Video.png" height="20" width="20"></a>
+1 |[ArithmeticSlices](/March2022/Java/ArithmeticSlices.java)  | <a href="https://www.youtube.com/watch?v=qRHWAKCOLxM"><img alt="Solution" src="https://github.com/Sunchit/Coding-Decoded/blob/master/img/Video.png" height="20" width="20"></a>
 
 ## [February 2022 Leetcode Solution Playlist](https://www.youtube.com/playlist?list=PLEI-q7w3s9gS2qlM3JnCVkciH4k7op4q-)
 
