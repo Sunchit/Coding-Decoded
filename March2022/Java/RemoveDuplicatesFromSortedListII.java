66cc95618a37f59a13baf53869443de34a123d0a
Sunchit Dudeja <sunchitdudeja@gmail.com>
2022-03-20 22:40:43 +0530

    Day20

--- March2022/Java/RemoveDuplicatesFromSortedListII.java
+++ March2022/Java/RemoveDuplicatesFromSortedListII.java
@@ -1,38 +1,34 @@
-
-// @saorav21994
-// TC : O(n)
-// SC : O(1)
-
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
+
+    // TC : O(n)
+    // SC : O(1)
     public ListNode deleteDuplicates(ListNode head) {
-        if (head == null || head.next == null) return head;
-        
-        ListNode prev = new ListNode(-1);
-        ListNode cur = prev;
-        
-        while (head != null) {
-            boolean duplicate = false;
-            while (head.next != null && head.val == head.next.val) {
-                head.next = head.next.next;
-                duplicate = true;
-            }
-            if (duplicate == false) {
-                prev.next = head;
-                prev = prev.next;
-            }
-            head = head.next;
+        if(head == null || head.next == null){
+            return head;
         }
-        prev.next = null;
-        return cur.next;
+
+        ListNode dummy = new ListNode(Integer.MIN_VALUE);
+        dummy.next = head;
+
+        ListNode it = head;
+        ListNode prev = dummy;
+
+        while(it!=null && it.next!=null){
+
+            boolean foundDuplication = false;
+            while(it!=null && it.next!=null && it.val == it.next.val){
+                foundDuplication  = true;
+                it = it.next;
+            }
+
+            if(!foundDuplication){
+                prev = prev.next;
+            } else {
+                prev.next = it.next;
+            }
+
+            it = it.next;
+        }
+        return dummy.next;
     }
 }
