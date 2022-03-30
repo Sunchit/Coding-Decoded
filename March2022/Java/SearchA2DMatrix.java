69c974fe6dbc775ce82d2489fd7812ffa6e90f7c
romitdutta10 <46403346+romitdutta10@users.noreply.github.com>
2022-03-30 12:42:17 +0530

    Update SearchA2DMatrix.java

--- March2022/Java/SearchA2DMatrix.java
+++ March2022/Java/SearchA2DMatrix.java
@@ -25,3 +25,39 @@ class Solution {
         return false;
     }
 }
+
+// Author: @romitdutta10
+// O(Row + logColumn)
+// SC: O(1)
+// Problem: https://leetcode.com/problems/search-a-2d-matrix/
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+        int n = matrix.length;
+        int m = matrix[0].length;
+        
+        for(int i=0; i<n; i++) {
+            if(matrix[i][0] <= target && matrix[i][m-1] >= target) {
+                int low = 0;
+                int high = m-1;
+                
+                while(low <= high) {
+                    int mid = low + (high - low)/2;
+                    
+                    if(matrix[i][mid] == target) {
+                        return true;
+                    } else if(matrix[i][mid] > target) {
+                        high = mid - 1;
+                    } else {
+                        low = mid + 1;
+                    }
+                }
+            } else if(matrix[i][0] > target) {
+                return false;
+            }
+        }
+        
+        return false;
+    }
+}
