5f2ee2639e007386865c4fbe35e6fa74e7cf012a
Sunchit Dudeja <sunchitdudeja@gmail.com>
2022-04-13 12:14:53 +0530

    Update SpiralMatrixII.java

--- April2022/Java/SpiralMatrixII.java
+++ April2022/Java/SpiralMatrixII.java
@@ -23,3 +23,59 @@ class Solution {
         return ans;
     }
 }
+
+
+// TC : O(n2)
+// SC : O(n2)
+
+public class Solution {
+    public int[][] generateMatrix(int n) {
+
+        int[][] ans = new int[n][n];
+
+        if (n == 0) {
+            return ans;
+        }
+
+        int startRow = 0;
+        int endRow = n-1;
+        int startColumn = 0;
+        int endColumn = n - 1;
+
+        int count = 1;
+
+        while (startRow <= endRow && startColumn <= endColumn) {
+            // Move towards right
+            for (int j = startColumn; j <= endColumn; j ++) {
+
+                ans[startRow][j] = count++;
+            }
+            startRow++;
+
+            // Move towards down
+            for (int i = startRow; i <= endRow; i ++) {
+                ans[i][endColumn] = count++;
+            }
+            endColumn--;
+
+            if (startRow <= endRow) {
+                // Move towards left
+                for (int j = endColumn; j >= startColumn; j --) {
+                    ans[endRow][j] = count++;
+                }
+            }
+            endRow--;
+
+            if (startColumn <= endColumn) {
+                // Move towards top
+                for (int i = endRow; i >= startRow; i --) {
+                    ans[i][startColumn] = count++;
+                }
+            }
+            startColumn ++;
+        }
+
+        return ans;
+    }
+
+}
