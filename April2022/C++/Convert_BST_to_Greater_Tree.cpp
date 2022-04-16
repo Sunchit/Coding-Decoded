64d2daf200c8f3ddd1ec570217aeac960118f4b8
shyam2520 <shyam2562000@gmail.com>
2022-04-16 08:31:01 +0530

    April 16

---
+++ April2022/C++/Convert_BST_to_Greater_Tree.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int sum=0;
+    TreeNode* convertBST(TreeNode* root) {
+        if(!root) return NULL;
+        
+        convertBST(root->right);
+        sum+=root->val;
+        root->val=sum;
+        convertBST(root->left);
+        return root;
+    }
+    
+};
