9f0c250acb0323735f28364849a6c15d9b7d0d29
Sunchit Dudeja <sunchitdudeja@gmail.com>
2022-04-17 11:13:37 +0530

    Merge

--- April2022/Java/ConvertBSTToGreaterTree.java
+++ April2022/Java/ConvertBSTToGreaterTree.java
@@ -1,40 +1,24 @@
-
-// @saorav21994
-// TC : O(n)
-// SC : O(1) -> recursion internal stack
-// reverese inorder parse tree and keep on adding prev sum to current node value
-
-
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+// Author: Shobhit Behl (LC: shobhitbruh)
 class Solution {
-    
-    int sum = 0;
-    
-    public void reverseInorder(TreeNode root) {
-        if (root == null) 
-            return;
-        reverseInorder(root.right);
-        sum += root.val;
-        root.val = sum;
-        reverseInorder(root.left);
+    public TreeNode convertBST(TreeNode root) {
+       int v=rec(root,0);
+        return root;
     }
     
-    public TreeNode convertBST(TreeNode root) {
-        reverseInorder(root);
-        return root;
+    public int rec(TreeNode root,int add){
+        if(root==null){
+            return 0;
+        }
+        int r=rec(root.right,add);
+        if(root.right==null){
+            root.val+=add;
+        }else{
+            root.val+=r;
+        }
+        int l=rec(root.left,root.val);
+        if(root.left!=null){
+            return l;
+        }
+        return root.val;
     }
 }
