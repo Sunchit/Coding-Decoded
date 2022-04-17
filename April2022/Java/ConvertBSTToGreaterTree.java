b9e0b709d4ddd9682a710837793bc348fcca74ce
romitdutta10 <46403346+romitdutta10@users.noreply.github.com>
2022-04-17 13:40:09 +0530

    Update ConvertBSTToGreaterTree.java

--- April2022/Java/ConvertBSTToGreaterTree.java
+++ April2022/Java/ConvertBSTToGreaterTree.java
@@ -22,3 +22,41 @@ class Solution {
         return root.val;
     }
 }
+
+// Author: @romitdutta10
+// TC: O(n)
+// SC: O(log n)
+// Problem: https://leetcode.com/problems/convert-bst-to-greater-tree/
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int sum = 0;
+    public TreeNode convertBST(TreeNode root) {
+        if(root == null) {
+            return null;
+        }
+        
+        convertBST(root.right);
+        sum += root.val;
+        root.val = sum;
+        
+        convertBST(root.left);
+        return root;
+        
+    }
+}
