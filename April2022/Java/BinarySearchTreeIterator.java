52d5f147bc3669f9fec2d6e9884f6ce53eb78ab7
Sunchit Dudeja <sunchitdudeja@gmail.com>
2022-04-20 12:43:28 +0530

    Day20

--- April2022/Java/BinarySearchTreeIterator.java
+++ April2022/Java/BinarySearchTreeIterator.java
@@ -60,6 +60,31 @@ class BSTIterator {
     }
 }
 
+
+public class BSTIterator {
+
+	Stack<TreeNode> stack =  null ;
+	TreeNode current = null ;
+
+	public BSTIterator(TreeNode root) {
+		current = root;
+		stack = new Stack<> ();
+	}
+
+	public boolean hasNext() {
+		return !stack.isEmpty() || current != null;
+	}
+
+	public int next() {
+		while (current != null) {
+			stack.push(current);
+			current = current.left ;
+		}
+		TreeNode t = stack.pop() ;
+		current = t.right ;
+		return t.val ;
+	}
+}
 /**
  * Your BSTIterator object will be instantiated and called as such:
  * BSTIterator obj = new BSTIterator(root);
