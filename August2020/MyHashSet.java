08fd2112dfefb80cb57834f628154058292bc545
Sunchit Dudeja <sunchitdudeja@gmail.com>
2022-04-21 13:26:42 +0530

    Update MyHashSet.java

--- August2020/MyHashSet.java
+++ August2020/MyHashSet.java
@@ -1,5 +1,27 @@
 class MyHashSet {
 
+    boolean[] set = null;
+    /** Initialize your data structure here. */
+    public MyHashSet() {
+        set = new boolean[1000001];
+    }
+
+    public void add(int key) {
+        set[key] = true;
+    }
+
+    public void remove(int key) {
+        set[key] = false;
+    }
+
+    /** Returns true if this set contains the specified element */
+    public boolean contains(int key) {
+        return set[key];
+    }
+}
+
+class MyHashSet {
+
     // Hashset => 4(1), 10(2), 16(1) , 17(3)
 
     /*
