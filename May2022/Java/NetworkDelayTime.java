67931b0ae4bec790a3db41075a89914b42786d0e
romitdutta10 <46403346+romitdutta10@users.noreply.github.com>
2022-05-14 20:05:59 +0530

    Update NetworkDelayTime.java

--- May2022/Java/NetworkDelayTime.java
+++ May2022/Java/NetworkDelayTime.java
@@ -60,3 +60,59 @@ class Solution {
 		return map;
 	}
 }
+
+// Author: @romitdutta10
+// TC: O(N)
+// SC: O(N * N)
+// Problem: https://leetcode.com/problems/network-delay-time/
+class Solution {
+    public int networkDelayTime(int[][] times, int n, int k) {
+        int weights[][] = new int[n+1][n+1];
+        
+        for(int[] weight: weights) {
+            Arrays.fill(weight, -1);
+        }
+        
+        for(int[] time: times) {
+            weights[time[0]][time[1]] = time[2];
+        }
+        
+        
+        Set<Integer> visited = new HashSet<>();
+        
+        PriorityQueue<int[]> qu = new PriorityQueue<>((a,b) -> a[1] - b[1]);
+        
+        
+        qu.offer(new int[]{k, 0});
+        visited.add(k);
+        
+        while(!qu.isEmpty()) {
+            int size = qu.size();
+            
+            while(size-- > 0) {
+                int[] curr = qu.poll();
+                int node = curr[0];
+                int time = curr[1];
+                
+                
+                visited.add(node);
+                
+                if(visited.size() == n) {
+                    return time;
+                }
+                
+                int[] neighbours = weights[node];
+                
+                for(int i=0; i<neighbours.length; i++) {
+                    if(neighbours[i] >= 0 && !visited.contains(i)) {
+                        qu.offer(new int[]{i, time + neighbours[i]});
+                    }
+                }
+            }
+        }
+        
+        return -1;
+        
+        
+    }
+}
