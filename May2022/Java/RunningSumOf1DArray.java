6dae49866c00417eae2f873af1673bab3d49e82b
saorav21994 <saorav21994@users.noreply.github.com>
2022-06-01 09:54:03 +0530

    Create RunningSumOf1DArray.java
    Question -
    https://leetcode.com/problems/running-sum-of-1d-array/

---
+++ May2022/Java/RunningSumOf1DArray.java
@@ -0,0 +1,13 @@
+
+// @saorav21994
+// TC : O(n)
+// SC : O(1)
+
+class Solution {
+    public int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i-1];
+        }
+        return nums;
+    }
+}
