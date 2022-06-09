3891a4588a21ae5041af35735591cc3f493a4695
Sunchit Dudeja <sunchitdudeja@gmail.com>
2022-06-09 17:47:00 +0530

    Day9

--- June2022/Java/TwoSumIIInputArrayIsSorted.java
+++ June2022/Java/TwoSumIIInputArrayIsSorted.java
@@ -21,3 +21,41 @@ class Solution {
         return new int[] {-1,-1};
     }
 }
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int start = 0;
+        int end = numbers.length-1;
+        while(start<end){
+            if( numbers[start] + numbers[end] == target){
+                int[] ans = new int[] { start+1, end+1};
+                return ans;
+            } else if(numbers[start] + numbers[end] < target){
+                start++;
+            } else {
+                //numbers[start] + numbers[end] > target
+                end--;
+            }
+        }
+        return null;
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] ans = new int[2];
+        for(int i=0;i<nums.length;i++){
+            int el = nums[i];
+            if(map.containsKey(target-el)){
+                // 4 2
+                if(map.get(target-el)!=i)
+                {   ans[0] = map.get(target-el)+1;
+                    ans[1] = i+1;
+                    break;
+                }
+            }
+            map.put(el,i);
+        }
+        return ans;
+
+    }
+}
