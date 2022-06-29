0628e6f5cd0915fc54fd2c05f5342f437022072b
Sunchit Dudeja <sunchitdudeja@gmail.com>
2022-06-30 00:49:22 +0530

    Update QueueReconstructionbyHeight.java

--- June2022/Java/QueueReconstructionbyHeight.java
+++ June2022/Java/QueueReconstructionbyHeight.java
@@ -3,6 +3,7 @@ class Solution {
     // SC : O(1)
      public int[][] reconstructQueue(int[][] people) {
         Arrays.sort(people, (a, b) -> a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]);
+        // both arraylist and linkedlist works 
         List<int[]> list = new ArrayList<>();
         for (int[] person : people) {
 
