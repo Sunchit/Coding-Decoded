53ec582f1db09fe28c3203c35f799f97bb880432
saorav21994 <saorav21994@users.noreply.github.com>
2022-08-04 21:59:34 +0530

    Update MirrorReflection.java
    Question -
    https://leetcode.com/problems/mirror-reflection/

--- August2022/Java/MirrorReflection.java
+++ August2022/Java/MirrorReflection.java
@@ -19,3 +19,28 @@ class Solution {
 		return -1;
 	}
 }
+
+
+
+
+// @saorav21994
+// TC : O(log(max(p,q)))
+// SC : O(1)
+
+class Solution {
+    public int gcd(int a, int b) {
+        if (a == 0) {
+            return b;
+        }
+        return gcd(b%a, a);
+    }
+    public int mirrorReflection(int p, int q) {
+        int g = gcd(p, q);
+        p /= g;
+        p %= 2;
+        q /= g;
+        q %= 2;
+        if (p == 1 && q == 1) return 1;
+        return p == 1 ? 0 : 2;
+    }
+}
