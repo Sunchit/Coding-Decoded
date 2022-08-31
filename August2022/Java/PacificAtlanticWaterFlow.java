e5c193cc7d207e99f20c5039a3bc317bea2a1fa3
saorav21994 <saorav21994@users.noreply.github.com>
2022-08-31 21:41:39 +0530

    Update PacificAtlanticWaterFlow.java
    Question -
    https://leetcode.com/problems/pacific-atlantic-water-flow/

--- August2022/Java/PacificAtlanticWaterFlow.java
+++ August2022/Java/PacificAtlanticWaterFlow.java
@@ -40,3 +40,99 @@ class Solution {
         return path;
     }
 }
+
+
+//===============================================================================================================================================================
+
+
+
+// @saorav21994
+// TC : O(m*n)
+// SC : O(m*n)
+
+class Solution {
+    
+    int [] abs = {0, 0, 1, -1};
+    int [] ord = {1, -1, 0, 0};
+    
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        
+        List<List<Integer>> res = new ArrayList<>();
+        
+        int m = heights.length;
+        int n = heights[0].length;
+        
+        boolean [][] visitPacific = new boolean[m][n];
+        boolean [][] visitAtlantic = new boolean[m][n];
+        
+        Queue<Pair> queue = new LinkedList<>();
+        
+        // first traverse for Pacific Ocean
+        
+        // first row and first column respectively to be added to queue as they are directly reachable
+        for (int i = 0; i < n; i++)
+            queue.offer(new Pair(0, i));
+        for (int i = 0; i < m; i++) 
+            queue.offer(new Pair(i, 0));
+        
+        bfs(queue, heights, visitPacific);
+        
+        // second traverse for Atlantic Ocean
+        
+        // last row and last column respectively to be added to queue as they are directly reachable
+        for (int i = 0; i < n; i++)
+            queue.offer(new Pair(m-1, i));
+        for (int i = 0; i < m; i++) 
+            queue.offer(new Pair(i, n-1));
+        
+        bfs(queue, heights, visitAtlantic);
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (visitPacific[i][j] && visitAtlantic[i][j]) {
+                    List<Integer> sublist = new ArrayList<>();
+                    sublist.add(i);
+                    sublist.add(j);
+                    res.add(new ArrayList<>(sublist));
+                }
+            }
+        }
+        
+        return res;       
+        
+    }
+    
+    
+    public void bfs(Queue<Pair> queue, int [][] heights, boolean [][] visited) {
+        
+        while (!queue.isEmpty()) {
+            
+            Pair cur = queue.poll();
+            
+            visited[cur.x][cur.y] = true;
+            
+            for (int i = 0; i < 4; i++) {
+                int x = cur.x+abs[i];
+                int y = cur.y+ord[i];
+                
+                if (x >= 0 && x < heights.length && y >= 0 && y < heights[0].length && !visited[x][y] && heights[x][y] >= heights[cur.x][cur.y]) {
+                    queue.offer(new Pair(x, y));
+                }
+            }
+            
+        }
+        
+    }
+    
+    
+    class Pair {
+        
+        int x;
+        int y;
+        
+        public Pair(int _x, int _y) {
+            this.x = _x;
+            this.y = _y;
+        }
+    }
+}
