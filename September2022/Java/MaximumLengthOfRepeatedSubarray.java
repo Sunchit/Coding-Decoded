7949d07eec68f80443fd1f76271a8d8982382f94
Anuj Chauhan <apc.mnnit@gmail.com>
2022-09-20 22:03:44 +0530

    Update MaximumLengthOfRepeatedSubarray.java

--- September2022/Java/MaximumLengthOfRepeatedSubarray.java
+++ September2022/Java/MaximumLengthOfRepeatedSubarray.java
@@ -52,3 +52,25 @@ class Solution {
         return res;
     }
 }
+
+
+============================ Clean code ============================
+    // author: anuj0503
+    class Solution {
+    public int findLength(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+        int[][] dp = new int[n+1][m+1];
+        int ans = 0;
+        
+        for(int i = 1; i <= n; i++)
+            for(int j = 1; j <= m; j++){
+                if(nums1[i-1] == nums2[j-1]){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                    ans = Math.max(ans, dp[i][j]);
+                }
+            }
+        
+        return ans;
+    }
+}
