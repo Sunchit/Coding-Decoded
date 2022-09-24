8469d097bbb2030078dc4f338dc071fc4ebb6ed4
saorav21994 <saorav21994@users.noreply.github.com>
2022-09-24 15:26:17 +0530

    Update PathSumII.java
    Question -
    https://leetcode.com/problems/path-sum-ii/
    
    @Sunchit Request you to please have a look at - https://github.com/Sunchit/Coding-Decoded/issues/504#issuecomment-1254146602

--- September2022/Java/PathSumII.java
+++ September2022/Java/PathSumII.java
@@ -41,3 +41,43 @@ class Solution {
 
 	}
 }
+
+
+
+// @saorav21994
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> res = new ArrayList<>();
+        dfs(root, targetSum, res, new ArrayList<Integer>());
+        return res;
+    }
+    
+    private void dfs(TreeNode root, int target, List<List<Integer>> res, List<Integer> cur) {
+        if (root == null) 
+            return;
+        if (root.left == null && root.right == null && root.val == target) {
+            cur.add(root.val);
+            res.add(cur);
+            return;
+        }
+        cur.add(root.val);
+        dfs(root.left, target-root.val, res, new ArrayList<>(cur));
+        dfs(root.right, target-root.val, res, new ArrayList<>(cur));
+    }
+}
