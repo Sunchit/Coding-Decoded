524ab0c234a1500664f9a155010d7257e4719836
Denzil Samuel <71846487+samueldenzil@users.noreply.github.com>
2022-10-04 22:44:47 +0530

    Update PathSum.cpp

--- October2022/C++/PathSum.cpp
+++ October2022/C++/PathSum.cpp
@@ -1,30 +1,23 @@
 // https://leetcode.com/problems/path-sum/
-class Solution {
-private:
-    bool helper(TreeNode* root, int target) {
-        if (root == NULL) {
-            return false;
-        }
-        
-        if (root->left == NULL && root->right == NULL) {
-            if (target - root->val == 0) {
-                return true;
-            }
-            return false;
-        }
-        bool left = helper(root->left, target - root->val);
-        bool right = false;
-        if (!left) {
-            right = helper(root->right, target - root->val);
-        }
-        
-        return left || right;
-    }
-    
+class Solution {    
 public:
     // TC: O(n)
     // SC: O(logn)
     bool hasPathSum(TreeNode* root, int targetSum) {
-        return helper(root, targetSum);
+        if (root == NULL) {
+            return false;
+        }
+        
+        if (root->left == NULL && root->right == NULL) {    // leaf node
+            return target - root->val == 0;
+        }
+        
+        bool left = hasPathSum(root->left, target - root->val);
+        bool right = false;
+        if (!left) {
+            right = hasPathSum(root->right, target - root->val);
+        }
+        
+        return left || right;
     }
 };
