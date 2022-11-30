fde0d60c1f2539b85439c3c7b10126f9c705d64c
saorav21994 <saorav21994@users.noreply.github.com>
2022-11-30 16:10:46 +0530

    Create UniqueNumberOfOccurances.java
    Question - 
    https://leetcode.com/problems/unique-number-of-occurrences/description/

---
+++ November2022/Java/UniqueNumberOfOccurances.java
@@ -0,0 +1,21 @@
+// @saorav21994
+
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        Set<Integer> set = new HashSet<>();
+        for (Integer key : map.keySet()) {
+            int val = map.get(key);
+            if (set.contains(val))
+                return false;
+            set.add(val);
+        }
+
+        return true;
+    }
+}
