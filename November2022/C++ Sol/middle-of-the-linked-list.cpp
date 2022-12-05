00db7df817e86f0e4cc50d0caed6cacfc4b7b88a
Sarang Kulkarni <sarangkulkarni567@gmail.com>
2022-12-05 15:09:54 +0530

    Create middle-of-the-linked-list.cpp

---
+++ November2022/C++ Sol/middle-of-the-linked-list.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        
+        ListNode*slow = head;
+        ListNode*fast = head;
+        
+        while(fast!=NULL && fast->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        return slow;
+     }
+};
