65aaa38578685bf0879555adaa5f2f89378431aa
shyam2520 <shyam2562000@gmail.com>
2023-02-19 11:21:38 +0530

    Feb 19th - Daily Leetcode

--- February2023/C++/ZigZagTraversal.cpp
+++ February2023/C++/ZigZagTraversal.cpp
@@ -13,36 +13,26 @@ class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         if(!root) return {};
-        deque<TreeNode*> q;
+        queue<TreeNode*> q;
         vector<vector<int>> res;
-        vector<int> temp;
-        q.push_back(root);
+        // vector<int> temp;
+        q.push(root);
         int size,level=0;
-        TreeNode* curr=NULL;
-        while(!q.empty())
+        while(q.size())
         {
-            size= q.size();
+            size=q.size();
+            vector<int> temp(size);
             for(int i=0;i<size;i++)
             {
-                if(level%2)
-                {
-                    curr=q.back();
-                    q.pop_back();
-                    temp.push_back(curr->val);
-                    if(curr->right) q.push_front(curr->right);
-                    if(curr->left) q.push_front(curr->left);
-                }
-                else
-                {
-                    curr=q.front();
-                    q.pop_front();
-                    temp.push_back(curr->val);
-                    if(curr->left) q.push_back(curr->left);
-                    if(curr->right) q.push_back(curr->right);
-                }
+                // temp.push_back(q.front()->val);
+                if(level%2) temp[size-i-1]=q.front()->val;
+                else temp[i]=q.front()->val;
+                if(q.front()->left) q.push(q.front()->left);
+                if(q.front()->right) q.push(q.front()->right);
+                q.pop(); 
             }
+            // if(level%2) reverse(begin(temp),end(temp));
             res.push_back(temp);
-            temp.clear();
             level++;
         }
         return res;
