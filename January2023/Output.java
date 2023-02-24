31e0d94132a7fe7499c15e0fed48ac40438236f7
Mahesh Reddy <nagamaheshreddy3@gmail.com>
2023-02-24 12:57:31 +0530

    Moved Jan & Feb23 to LC23

--- January2023/Output.java
+++
@@ -1,7 +0,0 @@
-public class Output {
-    public static void main(String[] args) {
-        System.out.println("Hello Everyone,\n My Name is Ankit Sharma\n The Haryanvi Businessman");
-       
-
-    }
-}
