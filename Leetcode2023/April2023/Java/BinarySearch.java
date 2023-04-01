83ca6b8d119b17394ca457a38d55fa9ead1a8ee8
Denzil Samuel <71846487+samueldenzil@users.noreply.github.com>
2023-04-01 11:44:56 +0530

    Create BinarySearch

---
+++ Leetcode2023/April2023/Java/BinarySearch.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        
+        return -1;
+    }
+}
