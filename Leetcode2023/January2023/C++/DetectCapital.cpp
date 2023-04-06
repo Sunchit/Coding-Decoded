9ca873b6631ab96742c88e6d87ab5e1c4dd7b1cb
Sunchit Dudeja <sunchitdudeja@gmail.com>
2023-04-07 01:19:18 +0530

    Correcting the repo

--- Leetcode2023/January2023/C++/DetectCapital.cpp
+++ Leetcode2023/January2023/C++/DetectCapital.cpp
@@ -1,29 +1,14 @@
-// https://leetcode.com/problems/detect-capital/
 class Solution {
 public:
-    // TC: O(n)
-    // SC: O(1)
     bool detectCapitalUse(string word) {
+        
         int n = word.length();
-        int capsCount = 0;
-        int lastCapsPosition = -1;
-        
-        for (int i = 0; i < n; i++) {
-            char c = word[i];
-            if (c - 'A' >= 0 && c - 'A' < 26) {
-                capsCount++;
-                lastCapsPosition = i;
-            }
+        int c = 0;
+        for(int i=0;i<n;i++)
+        {
+            if(word[i]>=65 and word[i]<=92) c++;
         }
-        
-        if (capsCount == n) {   // case 1
-            return true;
-        } else if (capsCount == 0 && lastCapsPosition == -1) {   // case 2
-            return true;
-        } else if (capsCount == 1 && lastCapsPosition == 0) {  // case 3
-            return true;
-        }
-        
+        if(c==n or c==0 or ((word[0]>=65 and word[0]<=92) and c==1)) return true;
         return false;
     }
 };
